2d5ecb8aa4b64c2a9ebef2285c15ffbcea8ee26f
gopal gautam <gopalgautam05@gmail.com>
2024-06-27 10:41:50 -0400

    Fixed the skeletons

--- src/components/blogCard/tutorialCardSkeleton.js
+++ src/components/blogCard/tutorialCardSkeleton.js
@@ -3,12 +3,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function TutorialCardSkeleton() {
   return (
-    <div className="flex flex-wrap">
-      {Array.from({ length: 10 }).map((_, index) => (
-        <div
-          key={index}
-          className="flex flex-col space-y-3 p-2 w-full h-full sm:w-1/2 lg:w-1/3"
-        >
+    <div className="">
+      {Array.from({ length: 6 }).map((_, index) => (
+        <div key={index} className="flex flex-col space-y-3 p-2 w-full h-full">
           <Skeleton className="h-[200px] w-full rounded-xl" />
           <div className="space-y-2">
             <Skeleton className="h-4 w-full" />
